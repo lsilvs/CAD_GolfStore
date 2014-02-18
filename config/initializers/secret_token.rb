79dfe76c99d3819c8e92850ac0874b86b70e5bc2
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-02-18 22:40:53 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfstore::Application.config.secret_token = '6da816f4e2eeb5b32072655e4fe047517dc7e4a7605f2010b5d4218382be85918b9b46679e63d9f210961597c14468d66eab31c751d52fc22e9daa7a1f6ec6a3'
